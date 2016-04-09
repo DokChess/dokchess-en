138faad163484d45eb5df739b458199fba09f616
Stefan Zoerner <stefan.zoerner@embarc.de>
2016-04-09 20:41:16 +0200

    Colour as a first type to test the GitHub repo.

---
+++ src/main/java/org/dokchess/domain/Colour.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2010-2016 Stefan Zoerner
+ *
+ * This file is part of DokChess.
+ *
+ * DokChess is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DokChess is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DokChess.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dokchess.domain;
+
+/**
+ * Colours of the two opponents as enumeration.
+ *
+ * @author StefanZ
+ */
+public enum Colour {
+
+    /** dark-coloured pieces
+     */
+    BLACK,
+
+    /** light-coloured pieces
+     */
+    WHITE;
+
+    /**
+     * Returns the other colour for a colour.
+     *
+     * @return the other colour.
+     */
+    public Colour otherColour() {
+        switch (this) {
+            case WHITE:
+                return BLACK;
+            case BLACK:
+                return WHITE;
+            default:
+                throw new IllegalStateException("Invalid colour");
+        }
+    }
+}
